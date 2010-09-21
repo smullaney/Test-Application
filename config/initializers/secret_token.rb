89e0034ff5b320558eb1a524400a408ca9097794
Sean Mullaney <spmullaney@gmail.com>
2010-09-21 12:46:26 +0100

    Starting the Development now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SparkOpsV10::Application.config.secret_token = '9209f1ca7636fa8f91adb9ed7b4bbbe1961b1d2e64392ba2577300f894993e52bcaec28fa935e4a747dcb433a6852951ec417c102257e721a7b530d736b27569'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SparkOpsV10::Application.config.session_store :cookie_store, :key => '_spark_ops_v1.0_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# SparkOpsV10::Application.config.session_store :active_record_store
